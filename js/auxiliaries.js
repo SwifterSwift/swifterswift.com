977794fc6bb73cc5d7f23863cb849fbcd8caec38
Bashar Ghadanfar <10.tens@gmail.com>
2017-02-17 15:39:31 +0300

    fix phone menu

--- js/auxiliaries.js
+++ js/auxiliaries.js
@@ -5,3 +5,13 @@ $(function () {
   $('[data-toggle="tooltip"]').tooltip()
   $('[data-toggle="popover"]').popover()
 })
+
+// Menu Button collapse on menu item click
+jQuery(document).ready(function($){
+	$('.navbar-nav > li > a').click(function () {
+		$('#navbar').removeClass('in')
+		$('#header .navbar .navbar-toggle').addClass('collapsed')
+	});
+	
+});
+
